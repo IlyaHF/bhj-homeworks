415ac6afa088cfec96882907cbbdfb57f9f5fb64
IlyaHF <xal19.90@mail.ru>
2023-07-18 20:17:29 +0300

    event-object homeWork

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,17 @@
+let menuTitle = document.querySelector('.dropdown__value');
+let menuList = document.querySelectorAll('.dropdown__link');
+
+function closeAndOpenMenu() {
+  let listOfMenu = document.querySelector('.dropdown__list')
+  listOfMenu.classList.contains('dropdown__list_active') ? listOfMenu.classList.remove('dropdown__list_active') : listOfMenu.classList.add('dropdown__list_active');
+};
+
+menuTitle.addEventListener('click', closeAndOpenMenu);
+
+menuList.forEach(function (item) {
+  item.onclick = function () {
+    document.querySelector('.dropdown__value').textContent = item.textContent;
+    return false;
+  };
+  item.addEventListener('click', closeAndOpenMenu);
+});
