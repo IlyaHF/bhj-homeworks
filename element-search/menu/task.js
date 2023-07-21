aedd35ceb9d8063f617e35a6c72cb2e91f8e9549
IlyaHF <xal19.90@mail.ru>
2023-07-21 18:30:59 +0300

    dzz

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -3,9 +3,12 @@ const menuLinks = document.querySelectorAll('.menu__link');
 menuLinks.forEach((element) => {
   element.addEventListener('click', (event) => {
     event.preventDefault();
-    const menu = element.nextElementSibling
-    if(menu.classList.contains('menu_sub')) {
+    const menu = element.nextElementSibling;
+
+    if(menu) {
       menu.classList.toggle('menu_active')
+    } else {
+      window.location.href = element.href;
     }
   })
 })
