8bce4ae16a23bb154b5c6ef8fa1c9c4c72db6afd
IlyaHF <xal19.90@mail.ru>
2023-08-03 18:54:42 +0300

    dz structure

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,25 @@
+const hasTooltip = document.querySelectorAll('.has-tooltip');
+
+function setTooltipActive(event) {
+   const tooltipActive = document.querySelector('.tooltip_active');
+   event.preventDefault();
+
+   if (tooltipActive) {
+      tooltipActive.classList.remove('tooltip_active');
+      if (tooltipActive.textContent == this.title) {
+         return false;
+      };
+   };
+
+   const tooltip = document.createElement('div');
+   tooltip.classList.add('tooltip', 'tooltip_active');
+   tooltip.textContent = this.getAttribute('title');
+   const coordinates = this.getBoundingClientRect();
+   tooltip.style.top = coordinates.top + coordinates.height + 4 + 'px';
+   tooltip.style.left = coordinates.left + 'px';
+   document.body.appendChild(tooltip);
+};
+
+for (const element of hasTooltip) {
+   element.addEventListener('click', setTooltipActive);
+};
